e7dcee63ca039a9c7fac3bd8e06e90ba3847134c
LATIsi <p.kmj9911@gmail.com>
2025-01-19 20:34:29 +0900

    STYLE : 장바구니 담기 갯수 레이아웃 완성

--- code/src/components/Nav.js
+++ code/src/components/Nav.js
@@ -4,6 +4,8 @@ import { AiOutlineShopping } from "react-icons/ai";
 import { IoArrowBackSharp } from "react-icons/io5";
 
 function nav(props) {
+    let cart_count = 5;
+
     return (
         <div id = "Nav">
             <ul>
@@ -16,12 +18,21 @@ function nav(props) {
                     </a>
                 </li> */}
                 <li className="cart_btn" >
+                    <div>
+                        {cart_count === 0? (
+                            <></>
+                        ) : (
+                            <div class="cart_count_circle">{cart_count}</div>
+                        )}
+                    </div>
+                    
                     <a href="">
                         <AiOutlineShopping size={35} color="white"
                         onMouseOver={({target})=>target.style.color="dimgrey"}
                         onMouseOut={({target})=>target.style.color="white"}
                         />
                     </a>
+                    
                 </li>
             </ul> 
         </div>
