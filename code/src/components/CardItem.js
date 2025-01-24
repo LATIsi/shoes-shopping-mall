d1b12436cfe6e79a87b064172e6815c6cf69ecdc
LATIsi <p.kmj9911@gmail.com>
2025-01-25 04:03:03 +0900

    STYLE : CardItem에 구매 버튼 추가

--- code/src/components/CardItem.js
+++ code/src/components/CardItem.js
@@ -1,13 +1,14 @@
 import React, { useState, useContext } from 'react';
 import  '../styles/card.css';
 import CartContext from '../context/CartContext';
+import CardNumberInput from './CardNumberInput';
 
 function CardItem(props) {
   const gotItemid = Number(props.id);
   const gotItemcart = props.card_state;
   const [selectid , setSelectId] = useState(gotItemid);
   const [selectcart , setSelectCart] = useState(gotItemcart);
-  let content = null;
+  let content,buy = null;
 
   const { cart, updateCartItemQuantity } = useContext(CartContext);
 
@@ -18,16 +19,28 @@ function CardItem(props) {
       )
   }
 
+  function onClickCartHander2(){
+    return <CardNumberInput/>
+}
+
   console.log(selectid,selectcart)
   // 버튼에 나오는 content
   // 받아온 props중, card_state 상태가 true(담기)면 버튼의 className을 show-menu로
   // false(담김!) 이면 버튼의 className을 hide-menu로 바꿔주어 각각에 맞는 css를 보여줌 
-   content = <button
-              className={ selectcart ? "show-menu" : "hide-menu" }
-              // 버튼 누르면 값 바꿔줌
-              onClick={onClickCartHander}
+    content = <button
+                className={ selectcart ? "show-menu" : "hide-menu" }
+                // 버튼 누르면 값 바꿔줌
+                onClick={onClickCartHander}
+              >
+              {selectcart ? "담기" : "담김!"} 
+              </button>
+
+    buy = <button
+              className="buy_btn"
+              // 버튼 누르면 결제 카드 등록으로 이동
+              onClick={onClickCartHander2}
             >
-            {selectcart ? "담기" : "담김!"} 
+            구매
             </button>
 
     return (
@@ -43,7 +56,10 @@ function CardItem(props) {
                       <div className="cards_product_name">{props.product_name}</div>
                       <div className="cards_product_description">{props.product_description}</div>
                       <div className="cards_product_price">{props.product_price}</div>
-                      <div className="cartToggleBtn" >{content}</div>
+                      <div>
+                          {content}
+                          {buy}
+                      </div>
                     </div>
                 </li>
             );
---
+++ code/src/components/CardNumberInput.js
@@ -0,0 +1,8 @@
+import React, { createContext, useContext } from 'react';
+import  '../styles/cardnumberinput.css';
+
+function CardNumberInput() {
+    return <div>카드 추가 예요~</div>
+}
+
+export default CardNumberInput;
